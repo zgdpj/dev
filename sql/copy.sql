4fb4d591b2ee15c9a46c39537bc3237b2d965f24
Jie PAN <jie.pan.mail@gmail.com>
2013-11-08 21:52:09 +0100

    Delete copy.sql

--- sql/copy.sql
+++
@@ -1,3 +0,0 @@
-truncate table dtm_prod.pack_qty;
-
-copy dtm_prod.pack_qty from 's3://bi-data-us/PACK_QTY/PACK_QTY.csv.gz' CREDENTIALS 'aws_access_key_id=AKIAJ67WWAE5PDGKFOMQ;aws_secret_access_key=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' delimiter ';' GZIP IGNOREHEADER 1;
